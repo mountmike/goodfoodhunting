f9d5e2be6488c36039061dc677eb878ced8337ff
Michael <tharratt.michael@gmail.com>
2023-03-13 15:43:45 +1100

    adding memory store to stop leaks

--- server.js
+++ server.js
@@ -9,13 +9,8 @@ const dishController = require("./controllers/dish_controller")
 const userController = require("./controllers/user_controller")
 const sessionController = require("./controllers/session_controller")
 const session = require("express-session")
+const MemoryStore = require('memorystore')(session)
 const db = require("./db")
-
-
-
-// session setup
-
-
 
 // http methods = get post put patch delete`
 //
@@ -43,6 +38,10 @@ app.use(methodOverride)
 // app.use(logger)
 
 app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
     secret: process.env.SESSION_SECRET || 'Godogsgo',
     resave: false,
     saveUninitialized: true,
